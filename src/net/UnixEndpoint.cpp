986286a20d4ad59aa00f14e6ff494b06a5ab375b
wtz <wangtz0607@outlook.com>
2025-02-02 02:05:22 +0800

    Support Unix domain socket addresses in the abstract namespace

--- src/net/UnixEndpoint.cpp
+++ src/net/UnixEndpoint.cpp
@@ -18,16 +18,30 @@
 using namespace mq;
 
 UnixEndpoint::UnixEndpoint(const std::filesystem::path &path) : addr_{} {
-    CHECK(path.string().size() < sizeof(addr_.sun_path));
+    CHECK(strlen(path.c_str()) < sizeof(addr_.sun_path));
 
     addr_.sun_family = AF_UNIX;
-    strcpy(addr_.sun_path, path.c_str());
-    addrLen_ = offsetof(struct sockaddr_un, sun_path) + path.string().size() + 1;
+
+    if (path.c_str()[0] == '@') {
+        addr_.sun_path[0] = '\0';
+        strcpy(addr_.sun_path + 1, path.c_str() + 1);
+
+        addrLen_ = offsetof(struct sockaddr_un, sun_path) + strlen(path.c_str());
+    } else {
+        strcpy(addr_.sun_path, path.c_str());
+
+        addrLen_ = offsetof(struct sockaddr_un, sun_path) + strlen(path.c_str()) + 1;
+    }
 }
 
 std::filesystem::path UnixEndpoint::path() const {
     size_t len = addrLen_ - offsetof(struct sockaddr_un, sun_path);
-    return std::filesystem::path(addr_.sun_path, addr_.sun_path + len);
+
+    if (addr_.sun_path[0] == '\0') {
+        return std::filesystem::path("@").concat(addr_.sun_path + 1, addr_.sun_path + len);
+    } else {
+        return std::filesystem::path(addr_.sun_path, addr_.sun_path + len - 1);
+    }
 }
 
 std::string UnixEndpoint::format() const {
