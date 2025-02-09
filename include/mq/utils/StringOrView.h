4de77bb8e4239f4934ef2a218860245d1d38c965
wtz <wangtz0607@outlook.com>
2025-02-09 23:10:45 +0800

    Make StringOrView a specialization of BasicStringOrView

--- include/mq/utils/StringOrView.h
+++ include/mq/utils/StringOrView.h
@@ -9,30 +9,37 @@
 
 namespace mq {
 
-class StringOrView {
+template <typename Char, typename Traits = std::char_traits<Char>>
+class BasicStringOrView {
 public:
-    constexpr StringOrView() : value_(std::string_view()) {}
+    constexpr BasicStringOrView()
+        : value_(std::basic_string_view<Char, Traits>()) {}
 
-    constexpr StringOrView(std::string &&value) : value_(std::move(value)) {}
+    constexpr BasicStringOrView(std::basic_string<Char, Traits> &&value)
+        : value_(std::move(value)) {}
 
-    constexpr StringOrView(const std::string &value) : value_(std::string_view(value)) {}
+    constexpr BasicStringOrView(const std::basic_string<Char, Traits> &value)
+        : value_(std::basic_string_view<Char, Traits>(value)) {}
 
-    constexpr StringOrView(std::string_view value) : value_(value) {}
+    constexpr BasicStringOrView(std::basic_string_view<Char, Traits> value)
+        : value_(value) {}
 
-    constexpr StringOrView(const char *value) : value_(std::string_view(value)) {}
+    constexpr BasicStringOrView(const char *value)
+        : value_(std::basic_string_view<Char, Traits>(value)) {}
 
-    constexpr StringOrView(const char *value, size_t size) : value_(std::string_view(value, size)) {}
+    constexpr BasicStringOrView(const char *value, size_t size)
+        : value_(std::basic_string_view<Char, Traits>(value, size)) {}
 
-    constexpr operator std::string() const & {
-        return std::visit([](const auto &value) { return std::string(value); }, value_);
+    constexpr operator std::basic_string<Char, Traits>() const & {
+        return std::visit([](const auto &value) { return std::basic_string<Char, Traits>(value); }, value_);
     }
 
-    constexpr operator std::string() && {
-        return std::visit([](auto &value) { return std::string(std::move(value)); }, value_);
+    constexpr operator std::basic_string<Char, Traits>() && {
+        return std::visit([](auto &value) { return std::basic_string<Char, Traits>(std::move(value)); }, value_);
     }
 
-    constexpr operator std::string_view() const {
-        return std::visit([](const auto &value) { return std::string_view(value); }, value_);
+    constexpr operator std::basic_string_view<Char, Traits>() const {
+        return std::visit([](const auto &value) { return std::basic_string_view<Char, Traits>(value); }, value_);
     }
 
     constexpr const char *data() const {
@@ -44,26 +51,28 @@ public:
     }
 
 private:
-    std::variant<std::string, std::string_view> value_;
+    std::variant<std::basic_string<Char, Traits>, std::basic_string_view<Char, Traits>> value_;
 };
+
+using StringOrView = BasicStringOrView<char>;
 
 } // namespace mq
 
-template <>
-struct std::hash<mq::StringOrView> {
-    constexpr size_t operator()(const mq::StringOrView &value) const noexcept {
-        return std::hash<std::string_view>{}(std::string_view(value));
+template <typename Char, typename Traits>
+struct std::hash<mq::BasicStringOrView<Char, Traits>> {
+    constexpr size_t operator()(const mq::BasicStringOrView<Char, Traits> &value) const noexcept {
+        return std::hash<std::basic_string_view<Char, Traits>>{}(std::basic_string_view<Char, Traits>(value));
     }
 };
 
-template <>
-struct std::formatter<mq::StringOrView> {
+template <typename Char, typename Traits>
+struct std::formatter<mq::BasicStringOrView<Char, Traits>> {
     constexpr auto parse(format_parse_context &ctx) {
         return ctx.begin();
     }
 
     template <typename FormatContext>
-    auto format(const mq::StringOrView &value, FormatContext &ctx) const {
-        return std::format_to(ctx.out(), "{}", std::string_view(value));
+    auto format(const mq::BasicStringOrView<Char, Traits> &value, FormatContext &ctx) const {
+        return std::format_to(ctx.out(), "{}", std::basic_string_view<Char, Traits>(value));
     }
 };
